e64bcb9b8499c9da41d626d03742ce71b1cb54d8
Carlos Jimenez <carlos.exejimenez@gmail.com>
2024-06-30 14:20:45 -0300

    Fix navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -13,10 +13,10 @@ const Navbar = () => {
   
   return (
     <Motion>
-      <nav className="flex flex-wrap items-center justify-between max-w-[65%] p-4 mx-auto md:py-8">
+      <nav className="w-full md:w-[68%] px-4 md:py-8 mx-auto sticky flex flex-wrap items-center justify-between">
         <div>
           <Link href={"#home"}>
-            <Image src={logo} alt="F3 Distribuciones" width={150} />
+            <Image src={logo} alt="F3 Distribuciones" width={100} />
           </Link>
         </div>
 
@@ -33,13 +33,13 @@ const Navbar = () => {
 
         <div
           onClick={() => setNav(!nav)}
-          className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+          className="cursor-pointer pr-4 z-20 md:hidden"
         >
-          {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+          {nav ? <FaTimes size={30} className="text-blanco" /> : <FaBars size={30} />}
         </div>
 
         {nav && (
-          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          <ul className="flex flex-col justify-center items-center absolute z-10 top-0 left-0 w-full h-screen bg-azul text-naranja">
             {links.map(({ id, label, url: link }) => (
               <li
                 key={id}
