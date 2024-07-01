36a5d9b4739093909e4decb3d4ee0bd8cc17d491
Carlos Jimenez <carlos.exejimenez@gmail.com>
2024-06-30 21:50:46 -0300

    add input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,42 @@
+import { InputProps } from "./Input.types";
+
+const Input: React.FC<InputProps> = ({ 
+  label,
+  placeholder,
+  type = 'text',
+  name,
+  id,
+  hasError = false,
+  required = false,
+  error,
+  handleChange,
+}) => {
+  return (
+    <div className="mb-4 w-full flex flex-col">
+      <label className="text-base text-oscuro" htmlFor={id}>{label}</label>
+      {type === 'textarea' ? (
+        <textarea 
+          className="mt-2 p-2 bg-transparent text-oscuro border-0 border-b-2 border-oscuro placeholder:text- focus:outline-none resize-none h-28"
+          name={name}
+          id={id}
+          placeholder={placeholder}
+          required={required}
+          onChange={handleChange}
+        ></textarea>
+      ) : (
+        <input
+          type={type}
+          name={name}
+          onChange={handleChange}
+          id={id}
+          required={required}
+          placeholder={placeholder}
+          className="mt-2 p-2 bg-transparent text-oscuro border-0 border-b-2 border-oscuro focus:outline-none"
+        />
+      )}
+      {hasError && (<p className="text-blanco text-xs font-light">{error}</p>)}
+    </div>
+  );
+};
+
+export default Input;
